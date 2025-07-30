12bd523e9ec96d19cab04c378149cf9e9441e1e3
Vitor Rodrigues <vpaulo@users.noreply.github.com>
2025-07-30 20:57:54 +0100

    removed requestAnimationFrame and added text wrap

--- src/main.js
+++ src/main.js
@@ -1,7 +1,27 @@
 window.addEventListener("load", (event) => {
   console.log(">> LOADED: ");
   const app = new App();
-  app.load();
+  app.load(`;(function(root) {
+    'use strict';
+
+    var KeyDownMonitor = function() {
+        this.keysDown = {};
+        var that = this;
+        window.addEventListener('keydown', function(e) {
+            that.keysDown[e.which || e.keyCode] = true;
+        });
+
+        window.addEventListener('keyup', function(e) {
+            that.keysDown[e.which || e.keyCode] = false;
+        });
+    };
+
+    KeyDownMonitor.prototype.isKeyDown = function(code) {
+        return this.keysDown[code];
+    };
+
+    root.KeyDownMonitor = KeyDownMonitor;
+}(window));`);
 });
 
 class App {
@@ -17,6 +37,7 @@ class App {
   defaultOptions = {
     fontSize: 16,
     lineHeight: 20,
+    letterSpacing: 2,
     // padding: 10,
     // gutterWidth: 50,
     // theme: 'dark',
@@ -33,7 +54,7 @@ class App {
   constructor(options) {
     console.log(">>> APP Options: ", options);
 
-    this.options = {...this.defaultOptions, ...options};
+    this.options = { ...this.defaultOptions, ...options };
 
     this.element = document.querySelector("#app");
     this.elRect = this.element.getBoundingClientRect();
@@ -44,13 +65,13 @@ class App {
     window.addEventListener("keydown", (e) => {
       console.log(">>> keydown: ", e, this.lines.length);
       switch (e.key) {
-        case "ArrowLeft": 
+        case "ArrowLeft": // TODO: shift + arrow for text selection
           if (this.cursor.col > 0) {
             this.cursor.col--;
-          } else if (this.cursor.col >= 0 && this.cursor.line > 0){
+          } else if (this.cursor.col >= 0 && this.cursor.line > 0) {
             this.cursor.line--;
             this.cursor.col = this.lines[this.cursor.line].length;
-          } 
+          }
           break;
         case "ArrowRight":
           if (this.lines[this.cursor.line] && this.lines[this.cursor.line].length > this.cursor.col) {
@@ -60,37 +81,39 @@ class App {
             this.cursor.col = 0;
           }
           break;
-        case "ArrowUp": 
+        case "ArrowUp":
           if (this.cursor.line > 0) {
             this.cursor.line--;
             this.cursor.col = this.lines[this.cursor.line].length < this.cursor.col ? this.lines[this.cursor.line].length : this.cursor.col;
-          } 
+          }
           break;
-        case "ArrowDown": 
+        case "ArrowDown":
           if (this.lines.length - 1 > this.cursor.line) {
             this.cursor.line++;
             this.cursor.col = this.lines[this.cursor.line].length < this.cursor.col ? this.lines[this.cursor.line].length : this.cursor.col;
           }
           break;
-        case "Backspace":
+        case "Backspace": // TODO ctrl + backspace to delete word
           if (this.cursor.col > 0) {
             const line = this.lines[this.cursor.line];
             this.lines[this.cursor.line] = line.slice(0, this.cursor.col - 1) + line.slice(this.cursor.col);
             this.cursor.col--;
-          } else if (this.lines.length > 1){
-            this.lines = this.lines.slice(0, this.lines.length - 1);
+          } else if (this.cursor.col === 0 && this.cursor.line > 0) {
+            const before = this.lines[this.cursor.line - 1];
+            this.lines[this.cursor.line - 1] += this.lines[this.cursor.line];
+            this.lines.splice(this.cursor.line, 1); // remove line
             this.cursor.line--;
-            this.cursor.col = this.lines[this.cursor.line].length;
-          } 
+            this.cursor.col = before.length;
+          }
           break;
         case "Delete":
           if (this.lines[this.cursor.line].length > this.cursor.col) {
             this.lines[this.cursor.line] = this.lines[this.cursor.line].slice(0, this.cursor.col) + this.lines[this.cursor.line].slice(this.cursor.col + 1);
-          } else if (this.lines[this.cursor.line + 1]) {
+          } else if (this.lines[this.cursor.line + 1] !== undefined && this.lines[this.cursor.line + 1] !== null) {
             const next = this.lines[this.cursor.line + 1];
             this.lines.splice(this.cursor.line + 1, 1); // remove line
             this.lines[this.cursor.line] += next; // append removed line contents
-          } 
+          }
           break;
         case "Enter":
           {
@@ -104,23 +127,31 @@ class App {
           }
           break;
         case "Tab": this.insertChar("  "); break; // TODO: shift + tab
-        default: this.insertChar(e.key);
+        default:
+          if (e.key.length < 2) {
+            this.insertChar(e.key);
+          }
+          break;
       }
+      this.loop();
       console.log(">>>> cursor: ", this.cursor);
       e.preventDefault();
     });
   }
 
   load(txt = "") {
-    this.lines = [...this.segLines.segment(txt)];
+    this.lines = [...this.segLines.segment(txt)].map(l => l.segment.replaceAll("\n", ""));
     if (this.lines.length === 0) {
       this.lines.push("");
     }
 
-    console.log(">>> APP Load: ", this.canvas, this.ctx, this.dpi, this.lines, this.segLines.segment(txt));
+    console.log(">>> APP Load: ", this.canvas, this.ctx, this.dpi, this.lines);
+
+
     this.observer = new ResizeObserver(() => {
       this.elRect = this.element.getBoundingClientRect();
       this.updateCanvasSize();
+      this.loop();
     });
     this.observer.observe(this.element);
 
@@ -142,7 +173,7 @@ class App {
   }
 
   loop() {
-    requestAnimationFrame(this.loop.bind(this));
+    // requestAnimationFrame(this.loop.bind(this));
 
     this.update();
     this.render();
@@ -152,40 +183,81 @@ class App {
   render() {
     this.ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
     this.ctx.font = `${this.options.fontSize}px Arial`;
+    this.ctx.letterSpacing = `${this.options.letterSpacing}px`;
     this.ctx.textBaseline = "bottom";
     this.ctx.fillStyle = "#000000";
 
-    const visibleLines = Math.floor(this.canvas.height / this.options.lineHeight);
-    const maxColumns = Math.floor(this.canvas.width / this.charWidth);
+    this.drawSelectedLine();
 
-    let height = this.options.lineHeight;
+    let offset = 0;
 
-    let items = this.lines.map(txt => {
-      let width = this.ctx.measureText(txt).width;
-      let item = {
-        txt,
-        width,
-      };
-      return item;
-    });
+    this.lines.forEach((line, i) => {
+      offset = i === 0 ? this.options.lineHeight : offset + this.options.lineHeight;
 
-    items.forEach((item, i) => {
-      let y = height * (i + 1);
-      this.ctx.fillText(item.txt, 0, y);
+      this.wrapText(line, offset)?.forEach((item) => {
+        this.ctx.fillText(item[0], 0, item[1]);
+        offset = item[1];
+      });
     });
     this.drawCursor();
   }
 
+  wrapText(ln, offset) {
+    let line = ''; // This will store the text of the current line
+    let testLine = ''; // This will store the text when we add a word, to test if it's too long
+    let lineArray = []; // This is an array of lines, which the function will return
+
+    const words = [...this.segWords.segment(ln)].map((word, i) => {
+      return {
+        value: word.segment,
+        width: this.ctx.measureText(word.segment).width,
+      };
+    });
+
+    // Lets iterate over each word
+    for (var n = 0; n < words.length; n++) {
+      // Create a test line, and measure it..
+      testLine += words[n].value;
+      // If the width of this test line is more than the max width
+      if (this.ctx.measureText(testLine).width > this.canvas.width && n > 0) {
+        // Then the line is finished, push the current line into "lineArray"
+        lineArray.push([line, offset]);
+        // Increase the line height, so a new line is started
+        offset += this.options.lineHeight;
+        // Update line and test line to use this word as the first word on the next line
+        line = words[n].value;
+        testLine = words[n].value;
+      }
+      else {
+        // If the test line is still less than the max width, then add the word to the current line
+        line += words[n].value;
+      }
+      // If we never reach the full max width, then there is only one line.. so push it into the lineArray so we return something
+      if (n === words.length - 1) {
+        lineArray.push([line, offset]);
+      }
+    }
+    // Return the line array
+    return lineArray;
+  }
+
   drawCursor() {
     const { line, col } = this.cursor;
+    const text = this.lines[line].slice(0, col);
     const y = line * this.options.lineHeight;
-    const x = col * this.charWidth;
+    const x = col === 0 ? 0 : this.ctx.measureText(text).width - this.options.letterSpacing;
+
     this.ctx.fillStyle = "#000000";
     this.ctx.fillRect(x, y, 2, this.options.lineHeight);
+  }
 
+  drawSelectedLine() {
+    this.ctx.save();
     this.ctx.globalAlpha = 0.5;
     this.ctx.fillStyle = "#cccccc";
-    this.ctx.fillRect(x, y, this.canvas.width, this.options.lineHeight);
+    this.ctx.fillRect(0, this.cursor.line * this.options.lineHeight, this.canvas.width, this.options.lineHeight);
+    this.ctx.restore();
+
   }
 
   insertChar(ch) {
@@ -194,92 +266,3 @@ class App {
     this.cursor.col += ch.length;
   }
 }
-
-
-// use Path2d objects , test it to see if it's going to work
-
-
-// main.js 
-// const canvas = document.getElementById('canvas');
-// const dpr = window.devicePixelRatio || 1;
-// const worker = new Worker('renderer.js');
-//
-// // Resize canvas and transfer control to worker
-// function resizeCanvas() {
-//   const width = window.innerWidth;
-//   const height = window.innerHeight;
-//
-//   canvas.width = width * dpr;
-//   canvas.height = height * dpr;
-//   canvas.style.width = width + "px";
-//   canvas.style.height = height + "px";
-//
-//   const offscreen = canvas.transferControlToOffscreen();
-//   worker.postMessage({
-//     type: 'init',
-//     canvas: offscreen,
-//     width,
-//     height,
-//     dpr
-//   }, [offscreen]);
-// }
-//
-// window.addEventListener('resize', () => {
-//   const width = window.innerWidth;
-//   const height = window.innerHeight;
-//   worker.postMessage({ type: 'resize', width, height });
-// });
-//
-// resizeCanvas();
-//
-// renderer.js
-// let canvas, ctx, width, height, dpr;
-// let img = new Image();
-// img.src = 'assets/example.png';
-//
-// onmessage = (e) => {
-//   const data = e.data;
-//
-//   if (data.type === 'init') {
-//     canvas = data.canvas;
-//     ctx = canvas.getContext('2d');
-//     width = data.width;
-//     height = data.height;
-//     dpr = data.dpr;
-//
-//     ctx.scale(dpr, dpr);
-//
-//     loop();
-//   }
-//
-//   if (data.type === 'resize') {
-//     width = data.width;
-//     height = data.height;
-//     canvas.width = width * dpr;
-//     canvas.height = height * dpr;
-//     ctx.scale(dpr, dpr);
-//   }
-// };
-//
-// function render() {
-//   ctx.clearRect(0, 0, width, height);
-//
-//   // Draw text
-//   ctx.fillStyle = '#0f0';
-//   ctx.font = `${Math.floor(width / 20)}px Arial`;
-//   ctx.fillText("OffscreenCanvas Worker", 50, 100);
-//
-//   // Draw image when ready
-//   if (img.complete) {
-//     const imgWidth = width / 4;
-//     const imgHeight = img.height * (imgWidth / img.width);
-//     ctx.drawImage(img, width - imgWidth - 20, 20, imgWidth, imgHeight);
-//   }
-// }
-//
-// function loop() {
-//   render();
-//   requestAnimationFrame(loop);
-// }
-//
-
